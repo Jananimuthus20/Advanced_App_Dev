46548982138f1f1be68f2a4dd79b35401c498a0f
Janani M <727821tucs059@skct.edu.in>
2024-03-15 16:16:35 +0530

    init

---
+++ ui_react/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Login from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
+import Weblayouts from './layouts/Weblayouts'
+
+const App = () => {
+  return (
+    <>
+     <BrowserRouter>
+     <Routes>
+      <Route>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      </Route>
+     </Routes>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ ui_react/src/layouts/Weblayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Weblayout = () => {
+  return (
+    <div>
+      Weblayout
+    </div>
+  )
+}
+
+export default Weblayout
